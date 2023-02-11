b06ea971285378ffdacbe300126f13cac8a2886f
Dany Sandoval <dantival4@hotmail.com>
2023-02-11 17:10:01 -0500

    API mostrar servicios

--- api/routes/routes.py
+++ api/routes/routes.py
@@ -1,17 +1,9 @@
 """Routes"""
 from fastapi import APIRouter
-<<<<<<< HEAD
 from schemas.login import CapturaDatos, CapturaDatos2, LoginUsuario, Mostrar
 from schemas.transferencia import Captura_transferencia, Realizar_tranfer
 from schemas.pagos import Captura_pago, Realizar_pago
-=======
-from schemas.login import CapturaDatos
-from schemas.login import CapturaDatos2
-from schemas.crear_cuenta import CapturaDatos3
-from schemas.login import LoginUsuario
-from schemas.login import Mostrar
-from schemas.crear_cuenta import CrearUsuario
->>>>>>> e673e78020639e9a0a19244ac3269b5dfc9242f6
+from schemas.crear_cuenta import CapturaDatos3, CrearUsuario
 import json
 from schemas.login import mostrar_servicios_publicos
 
@@ -55,14 +47,18 @@ async def view_datos(usuario: CapturaDatos2):
     except ExceptionCustumizada:
         return "El usuario no existe"
 
-<<<<<<< HEAD
 @mi_api_router.post("/transfer")
 async def view_datos(transferencia: Captura_transferencia):
     try:
         rta_mostrar = Realizar_tranfer(transferencia)
         informacion = rta_mostrar.ejecutar_transfer()
-=======
-<<<<<<< HEAD
+        if informacion == []:
+            raise ExceptionCustumizada('')
+        else:
+            return informacion
+    except ExceptionCustumizada:
+        return "transferencia fallida"
+
 @mi_api_router.post("/crear_usuario")
 async def view_crear_usuario(usuario2: CapturaDatos3):
     """Acceso"""
@@ -76,21 +72,20 @@ async def view_crear_usuario(usuario2: CapturaDatos3):
             return "Cuenta con celular "+str(informacion[0][0])+" creada correctamente"
     except ExceptionCustumizada:
         return "error"
-=======
+
 @mi_api_router.post("/mostrar_servicios_publicos")
 async def view_datos():
     """Acceso"""
     try:
         rta_servicios_publicos= mostrar_servicios_publicos()
         informacion = rta_servicios_publicos
->>>>>>> e673e78020639e9a0a19244ac3269b5dfc9242f6
         if informacion == []:
             raise ExceptionCustumizada('')
         else:
             return informacion
     except ExceptionCustumizada:
-<<<<<<< HEAD
-        return "transferencia fallida"
+        return "No existen convenios de servicios publicos"
+
 
 @mi_api_router.post("/pagos")
 async def view_datos(pago: Captura_pago):
@@ -103,7 +98,4 @@ async def view_datos(pago: Captura_pago):
             return informacion
     except ExceptionCustumizada:
         return "Pago fallido"
-=======
-        return "No existen convenios de servicios publicos"
->>>>>>> 1fc02435dfa9dbe3ec374a61df722226022ea81f
->>>>>>> e673e78020639e9a0a19244ac3269b5dfc9242f6
+
